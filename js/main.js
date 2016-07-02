63bac6d3c13fa2947f854ba249841ad9537bf21e
IvanDev <IvanDev>
2016-07-02 22:39:29 +0300

    commitchaa

--- js/main.js
+++ js/main.js
@@ -12,4 +12,55 @@ $(document).ready(function() {
         inherit_height_from:$('#main-banner'),
         inherit_width_from:$('#main-banner')
     });
+
+
+    $('.item-link').hover(
+
+        function(){
+            $(this).animate({'backgroundColor': '#f5f5f5'},400);
+        },
+        function(){
+            $(this).animate({'backgroundColor': '#000'},400);
+        }
+
+    );
+
+
+
+
+    $("#submitForNews").click(function(event) {
+        event.preventDefault();
+        if( $("#emailForNews").val().length === 0 ) {
+            $("#emailForNews").val('Введіть Вашу email адресу');
+        } else {
+            $.post( "app/ajaxParser.php",{type:'emailForNews', email: $("#emailForNews").val()}, function( data ) {
+                $( "#assignNews" ).slideDown( "slow" );
+                $("#emailForNews").val('');
+            })}
+    });
+
+
+
+    $("#submitForVolunteer").click(function(event) {
+        event.preventDefault();
+        if( $("#emailVolunteer").val().length === 0 ) {
+            $("#emailVolunteer").val('Введіть Вашу email адресу');
+        } else {
+            $.post( "app/ajaxParser.php",{type:'emailVolunteer', email: $("#emailVolunteer").val()}, function( data ) {
+                $( "#assignVolunteer" ).slideDown( "slow" );
+                $("#emailVolunteer").val('');
+            })}});
+
+    $(".activity-rectangle").click(function(){
+        var selector = $(this).attr("class").split(" ")[1];
+        if($("#hidden-details-" + selector).css("display") === "none"){
+            $(".hidden-details").slideUp(1000);
+            $(".activity-rectangle").removeClass("hidden-details-active");
+            $(this).addClass('hidden-details-active');
+            $("#hidden-details-" + selector).slideDown(1000);
+        } else{
+            $(this).removeClass("hidden-details-active");
+            $("#hidden-details-" + selector).slideUp(1000);
+        }
+    })
 });
