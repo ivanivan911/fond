a2f9a64e41784d413e19f93ee915d079552ccd93
IvanDev <IvanDev>
2016-07-02 14:20:26 +0300

    add slider

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by IvanDev on 02.07.2016.
+ */
+$(document).ready(function() {
+    $('#slides').superslides({
+        play: 30000,
+        slide_easing: 'easeInOutCubic',
+        slide_speed: 800,
+        pagination: true,
+        hashchange: true,
+        scrollable: true,
+        inherit_height_from:$('#main-banner')
+    });
+});
